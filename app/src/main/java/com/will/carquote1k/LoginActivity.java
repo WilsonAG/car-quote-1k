7d9b8102519c8690a9ef0a76c89e5dfa4cff7a8a
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-12-06 21:13:42 -0500

    init app

---
+++ app/src/main/java/com/will/carquote1k/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.will.carquote1k;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
